9436db7ce69c7b9e8cef784bfee79f346f593204
baptiste <bbichero@5d93ae139130>
2018-10-20 17:47:44 +0000

    add error controller

---
+++ controllers/error.js
@@ -0,0 +1,33 @@
+/**
+ * All API Errors Handler
+ *
+ * @param {*} err
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+
+module.exports = function (err, req, res, next) {
+
+	// When you add a custom error handler,
+	// we must delegate to the default Express error handler,
+	// when the headers have already been sent to the client.
+	//
+	// see: https://expressjs.com/en/guide/error-handling.html#the-default-error-handler
+	if (res.headersSent) {
+		return next(err)
+	}
+
+	// Parsing the error received.
+	let error = {
+		error: true,
+		statusCode: (err.statusCode) ? err.statusCode : 500,
+		message: (err.message) ? err.message : "That's all we know.",
+	};
+
+	// Doing stuff with logging the error.
+	// console.log(err)
+
+	// Returning the error to the client.
+	return res.status(error.statusCode).send(error);
+};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const requestService	= require("./api/request");
 const throwError	= require("./api/error");
+const errorController	= require("./controllers/error");
 const throwSuccess	= require("./api/success");
 const gitModule		= require("./git");
 const Ifetch		= require("./fetch/ifetch");
@@ -8,6 +9,7 @@ const fetchAPI		= require("./fetch/fetch-api");
 module.exports = {
 	request: requestService,
 	error: throwError,
+	errorController: errorController,
 	success: throwSuccess,
 	git: gitModule,
 	Ifetch: Ifetch,
