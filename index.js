30b05a8bd7666fd86f4cb17887a6fe2b22200c32
baptiste <bbichero@5d93ae139130>
2018-10-20 16:21:59 +0000

    add ifetch and edit api dir location

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const requestService	= require("./lib/request");
-const throwError	= require("./lib/error");
-const throwSuccess	= require("./lib/success");
+const requestService	= require("./api/request");
+const throwError	= require("./api/error");
+const throwSuccess	= require("./api/success");
 const gitModule		= require("./git");
+const Ifetch		= require("./fetch/ifetch");
 
 module.exports = {
 	request: requestService,
 	error: throwError,
 	success: throwSuccess,
 	git: gitModule,
+	Ifetch: Ifetch
 }
