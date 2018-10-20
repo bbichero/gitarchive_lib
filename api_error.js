e4528a2bafa3c133d4bccb15c01b4d78f9f585df
baptiste <bbichero@5d93ae139130>
2018-10-20 12:45:33 +0000

    all lib file from api

---
+++ api_error.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// Inspired from https://www.npmjs.com/package/boom
+module.exports = class CustomBoom extends Error {
+
+    constructor(err, options = {}) {
+
+        if (err instanceof Error) { return err; }
+
+		const { statusCode = 500, data = null } = options;
+		
+        const error = new Error(err ? err : undefined);   // Avoids settings null message
+        Error.captureStackTrace(error);                   // Filter the stack to our external API
+        error.data = data;
+		error.statusCode = statusCode;
+		error.message = err;
+		
+        return error;
+    }
+
+    // 4xx
+
+    static badRequest (message, data) {
+        return new CustomBoom(message, {statusCode: 400, data});
+    }
+
+    static unauthorized (message, data) {
+        return new CustomBoom(message, {statusCode: 401, data});
+    }
+
+    static notFound (message, data) {
+        return new CustomBoom(message, {statusCode: 404, data});
+    }
+
+    // 5xx
+
+    static badImplementation (message, data) {
+        // TODO. Message should be hidded.
+        return new CustomBoom(message, {statusCode: 500});
+    }
+
+    static notImplemented (message, data) {
+        return new CustomBoom(message, {statusCode: 501});
+    }
+
+}
---
+++ database.js
@@ -0,0 +1,55 @@
+const promise		= require('bluebird'); // or any other Promise/A+ compatible library;
+const IS_PRODUCTION	= (process.env.NODE_ENV == 'production');
+const config	    	= require("../config");
+const APIError = require('./api_error');
+
+const initOptions	= {
+	    promiseLib: promise // overriding the default (ES6 Promise);
+};
+const pgp		= require('pg-promise')(initOptions);
+
+// Database connection details;
+const cn		= {
+	host: config.services.db.hostname,
+	port: config.services.db.port,
+	database: config.services.db.database,
+	user: config.services.db.username,
+	password: config.services.db.password,
+    	ssl: true
+};
+
+// Instantiate connection
+const db = pgp(cn);
+
+// Surcharge db object witb a lib object...
+const Database = Object.assign({
+
+	lib: {
+		// ...with a custom errors handler,
+		handleErrors: function (err) {
+			if (err.statusCode)
+				throw err;
+			else
+				throw APIError.notImplemented(err);
+		},
+
+		// ...with a custom query limit.
+		sqlQueryLimit: function (limit) {
+			const SQL_DEFAULT_LIMIT = 25;
+			return (Number(limit) && limit > 0 && limit < SQL_DEFAULT_LIMIT * 10 ? limit : SQL_DEFAULT_LIMIT);
+		},
+
+		formatNumber: function (int) { return isNaN(int) ? 0 : int; },
+
+		formatDate: function (date) {
+			if (typeof date !== "undefined" && typeof date.getMonth == "function")
+				{ return date; }
+			
+			const dateObj = new Date(date);
+
+			return isNaN(dateObj.getTime()) ? null : dateObj.toISOString();
+		}
+	}
+}, db);
+
+module.exports = Database;
