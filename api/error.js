1cbab33c3f627ccb7f36b9f623856608c57475c6
baptiste bicheron <bbichero@student.42.fr>
2018-11-29 19:45:08 +0000

    add 503 error handling

--- api/error.js
+++ api/error.js
@@ -3,45 +3,49 @@
 // Inspired from https://www.npmjs.com/package/boom
 module.exports = class CustomBoom extends Error {
 
-    constructor(err, options = {}) {
+	constructor(err, options = {}) {
 
-        if (err instanceof Error) { return err; }
+		if (err instanceof Error) { return err; }
 
 		const { statusCode = 500, data = null } = options;
-		
-        const error = new Error(err ? err : undefined);   // Avoids settings null message
-        Error.captureStackTrace(error);                   // Filter the stack to our external API
-        error.data = data;
-	error.statusCode = statusCode;
-	error.message = err;
-	throw new CustomBoom(error);
 
-        return error;
-    }
+		const error = new Error(err ? err : undefined);   // Avoids settings null message
+		Error.captureStackTrace(error);                   // Filter the stack to our external API
+		error.data = data;
+		error.statusCode = statusCode;
+		error.message = err;
+		throw new CustomBoom(error);
 
-    // 4xx
+		return error;
+	}
 
-    static badRequest (message, data) {
-        return new CustomBoom(message, {statusCode: 400, data});
-    }
+	// 4xx
 
-    static unauthorized (message, data) {
-        return new CustomBoom(message, {statusCode: 401, data});
-    }
+	static badRequest (message, data) {
+		return new CustomBoom(message, {statusCode: 400, data});
+	}
 
-    static notFound (message, data) {
-        return new CustomBoom(message, {statusCode: 404, data});
-    }
+	static unauthorized (message, data) {
+		return new CustomBoom(message, {statusCode: 401, data});
+	}
 
-    // 5xx
+	static notFound (message, data) {
+		return new CustomBoom(message, {statusCode: 404, data});
+	}
 
-    static badImplementation (message, data) {
-        // TODO. Message should be hidded.
-        return new CustomBoom(message, {statusCode: 500});
-    }
+	// 5xx
 
-    static notImplemented (message, data) {
-        return new CustomBoom(message, {statusCode: 501});
-    }
+	static badImplementation (message, data) {
+		// TODO. Message should be hidded.
+		return new CustomBoom(message, {statusCode: 500});
+	}
+
+	static notImplemented (message, data) {
+		return new CustomBoom(message, {statusCode: 501});
+	}
+
+	static serviceUnavailable (message, data) {
+		return new CustomBoom(message, {statusCode: 503});
+	}
 
 }
