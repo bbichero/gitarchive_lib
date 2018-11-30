8f0cbf68d996adce320794791b5151ec7e8a1496
baptiste bicheron <bbichero@student.42.fr>
2018-11-30 14:57:57 +0000

    change console.log to console.error

--- controllers/error.js
+++ controllers/error.js
@@ -26,7 +26,7 @@ module.exports = function (err, req, res, next) {
 	};
 
 	// Doing stuff with logging the error.
-	console.log(err)
+	console.error("console err:", error)
 
 	// Returning the error to the client.
 	return res.status(error.statusCode).send(error);
